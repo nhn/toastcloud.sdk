65c01ebbcf86cb2c9f5c32434cefd47972dc68f1
김기협/TOAST클라이언트개발팀/NE <hyup1028@nhnent.com>
2018-03-16 19:14:57 +0900

    Update ToastSDK Sample Version 1.0.0

--- Sample/ToastSDKSample/ToastSDKSample/ViewController.h
+++ Sample/ToastSDKSample/ToastSDKSample/ViewController.h
@@ -8,8 +8,40 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+#import <ToastLogger/ToastLogger.h>
+#import <ToastCrash/ToastCrash.h>
 
+
+@interface ViewController : UIViewController <TCISLoggerDelegate, TCISCrashDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *userIDTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *userInfoKey1TextField;
+@property (weak, nonatomic) IBOutlet UITextField *userInfoKey2TextField;
+@property (weak, nonatomic) IBOutlet UITextField *userInfoKey3TextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *userInfoValue1TextField;
+@property (weak, nonatomic) IBOutlet UITextField *userInfoValue2TextField;
+@property (weak, nonatomic) IBOutlet UITextField *userInfoValue3TextField;
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *logLevelSegment;
+
+@property (weak, nonatomic) IBOutlet UITextField *tagTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *messageTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *logUserInfoKey1TextField;
+@property (weak, nonatomic) IBOutlet UITextField *logUserInfoKey2TextField;
+@property (weak, nonatomic) IBOutlet UITextField *logUserInfoKey3TextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *logUserInfoValue1TextField;
+@property (weak, nonatomic) IBOutlet UITextField *logUserInfoValue2TextField;
+@property (weak, nonatomic) IBOutlet UITextField *logUserInfoValue3TextField;
+
+@property (weak, nonatomic) IBOutlet UITextView *delegateTextView;
+
+- (IBAction)sendLogButton:(id)sender;
+- (IBAction)crashButton:(id)sender;
 
 @end
 
