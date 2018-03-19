4c0f140b9dc7674d7054f3fd7e86910088b94ed9
김기협/TOAST클라이언트개발팀/NE <hyup1028@nhnent.com>
2018-03-19 12:27:36 +0900

    Update ToastSDK Version 1.0.0

---
+++ ToastLogger/ToastLogger.framework/Headers/TCISLoggerConfigurationSetting.h
@@ -0,0 +1,66 @@
+//
+//  TCISLoggerConfigurationSetting.h
+//  ToastLogger
+//
+//  Created by Hyup on 2017. 10. 18..
+//  Copyright © 2017년 NHNEnt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "TCISLog.h"
+
+@interface TCISLoggerConfigurationSetting : NSObject
+
+typedef enum {
+    TCISLoggerConfigurationTypeDefault = 0,
+    TCISLoggerConfigurationTypeConsole,
+    TCISLoggerConfigurationTypeUser
+} TCISLoggerConfigurationType;
+
++ (instancetype)defaultSetting;
++ (instancetype)consoleSetting;
++ (instancetype)userSettingWithEnableNormalLog:(BOOL)enableNormalLog
+                                enableCrashLog:(BOOL)enableCrashLog
+                          enableLogLevelFilter:(BOOL)enableLogLevelFilter
+                                filterLogLevel:(TCISLogLevel)filterLogLevel
+                           enableLogTypeFilter:(BOOL)enableLogTypeFilter
+                            filterLogTypeArray:(NSArray *)filterLogTypeArray
+                      enableLogDuplicateFilter:(BOOL)enableLogDuplicateFilter
+                 filterLogDuplicateExpiredTime:(NSNumber *)filterLogDuplicateExpiredTime;
+
+- (TCISLoggerConfigurationType)configurationType;
+
+#pragma mark - URL
+- (NSString *)collectorServerURL;
+- (void)setCollectorServerURL:(NSString *)collectorServerURL;
+
+- (NSString *)collectorServerVersion;
+- (void)setCollectorServerVersion:(NSString *)collectorServerVersion;
+
+
+#pragma mark - Filter
+- (BOOL)isEnableLogDuplicateFilter;
+- (void)setEnableLogDuplicateFilter:(BOOL)enable;
+- (NSNumber *)filterLogDuplicateExpiredTime;
+- (void)setFilterLogDuplicateExpiredTime:(NSNumber *)filterLogDuplicateExpiredTime;
+
+- (BOOL)isEnableLogLevelFilter;
+- (void)setEnableLogLevelFilter:(BOOL)enable;
+- (TCISLogLevel)filterLogLevel;
+- (void)setFilterLogLevel:(TCISLogLevel)logLevel;
+
+- (BOOL)isEnableLogTypeFilter;
+- (void)setEnableLogTypeFilter:(BOOL)enable;
+- (NSArray *)filterLogType;
+- (void)setFilterLogType : (NSArray *)fiterLogType;
+
+#pragma mark - Enable, Disable
+- (BOOL)isEnableNormalLog;
+- (void)enableNormalLog:(BOOL) enable;
+
+- (BOOL)isEnableCrashLog;
+- (void)enableCrashLog:(BOOL) enable;
+
+@end
+
