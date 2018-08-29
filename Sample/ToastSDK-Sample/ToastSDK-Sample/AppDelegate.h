1ab29c018c0cbb9cdd5c7af489bcee0a65fdc768
김기협/TOAST클라이언트개발팀/NE <hyup1028@nhnent.com>
2018-08-29 13:53:23 +0900

    Add ToastSDK Sample

---
+++ Sample/ToastSDK-Sample/ToastSDK-Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToastSDK-Sample
+//
+//  Created by Hyup on 2018. 8. 29..
+//  Copyright © 2018년 NHN Entertainment Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Sample/ToastSDK-Sample/ToastSDK-Sample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToastSDK-Sample
+//
+//  Created by Hyup on 2018. 8. 29..
+//  Copyright © 2018년 NHN Entertainment Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
